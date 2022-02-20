87cd7a7cc4709b3daf3f606d5d8f15dab39b20e8
ytanriverdi <yasintanriverdi@hotmail.com>
2022-02-20 13:45:28 +0100

    Initial commit

---
+++ app/src/main/java/com/yasintanriverdi/composeintro/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.yasintanriverdi.composeintro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yasintanriverdi.composeintro.ui.theme.ComposeIntroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeIntroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeIntroTheme {
+        Greeting("Android")
+    }
+}
